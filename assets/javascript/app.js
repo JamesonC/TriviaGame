838dbd585e6f1fee7b606c71cdd77c2fbf8a8e9f
Jameson Campbell <jcampbell@flexterapp.com>
2019-04-01 22:07:20 -0500

    reconfigured object structure

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,79 +1,96 @@
-$(document).ready(function() {
-
-var question1 = {
-    text: "'The Mountain' is the nickname for which character?",
-    answer: "Gregor Clegane",
-    wrong: ["Sandor Clegane", "Gerold Clegane", "Oberyn Martell"],
-    correct: false,
-};
-
-var question2 = {
-    text: "Who was Margaery Tyrell's first husband?",
-    answer: "Renly Baratheon",
-    wrong: ["Tommen Baratheon", "Joffrey Baratheon", "Stannis Baratheon"],
-    correct: false,
-};
-
-var question3 = {
-    text: "Who is Lord Commander of the Kingsguard at the beginning of Game of Thrones?",
-    answer: "Ser Barristan Selmy",
-    wrong: ["Ser Jor Mormont", "Ser Loras Tyrell", "Ser Jamie Lannister"],
-    correct: false,
-};
-
-var question4 = {
-    text: "Who is known as 'The-King-Beyond-the-Wall'?",
-    answer: "Mance Rayder",
-    wrong: ["Stannis Baratheon", "Tormund Giantsbane", "The Night King"],
-    correct: false,
-};
-
-var question5 = {
-    text: "How many times has Sansa Stark been married?",
-    answer: "Twice",
-    wrong: ["Three Times", "Once", "None"],
-    correct: false,
-};
+$(document).ready(function () {
 
 
-var questionBank = [question1, question2, question3, question4, question5];
-var intervalId;
-var number = 30; // Number of seconds remaining
-var count = 0; // Keeps track of the index of the currently displaying question.
-var questionDetails = questionBank[count];
+    var question1 = {
+        text: "'The Mountain' is the nickname for which character?",
+        answer: [
+            "<div class='choices btn btn-primary'>Oberyn Martell</div>",
+            "<div class='choices btn btn-primary'>Gerold Clegane</div>",
+            "<div class='choices btn btn-primary'>Sandor Clegane</div>",
+            "<div class='choices btn btn-primary' data-correct='true'>Gregor Clegane</div>"
+        ],
+        correct: false,
+    };
 
+    var question2 = {
+        text: "Who was Margaery Tyrell's first husband?",
+        answer: [
+            "<div class='choices btn btn-primary' data-correct='true'>Renly Baratheon</div>",
+            "<div class='choices btn btn-primary'>Tommen Baratheon</div>",
+            "<div class='choices btn btn-primary'>Joffrey Baratheon</div>",
+            "<div class='choices btn btn-primary'>Stannis Baratheon</div>"
+        ],
+        correct: false,
+    };
 
-function displayQuestion() {
+    var question3 = {
+        text: "Who is Lord Commander of the Kingsguard at the beginning of Game of Thrones?",
+        answer: [
+            "<div class='choices btn btn-primary'>Ser Jor Mormont</div>",
+            "<div class='choices btn btn-primary' data-correct='true'>Ser Barristan Selmy</div>",
+            "<div class='choices btn btn-primary'>Ser Loras Tyrell</div>",
+            "<div class='choices btn btn-primary'>Ser Jamie Lannister</div>"
+        ],
+        correct: false,
+    };
 
-    $(`#question`).html(questionDetails.text);
-    $(`#answer-choices`).html("<div class='choices btn btn-primary'>" + questionDetails.answer);
+    var question4 = {
+        text: "Who is known as 'The-King-Beyond-the-Wall'?",
+        answer: [
+            "<div class='choices btn btn-primary'>Stannis Baratheon</div>",
+            "<div class='choices btn btn-primary' data-correct='true'>Mance Rayder</div>",
+            "<div class='choices btn btn-primary'>Tormund Giantsbane</div>",
+            "<div class='choices btn btn-primary'>The Night King</div>"
+        ],
+        correct: false,
+    };
 
-    for (var i = 0; i < questionDetails.wrong.length; i++) {
-        $(`#answer-choices`).append("<div class='choices btn btn-primary'>" + questionDetails.wrong[i]);
-    }
-}
+    var question5 = {
+        text: "How many times has Sansa Stark been married?",
+        answer: [
+            "<div class='choices btn btn-primary'>Once</div>",
+            "<div class='choices btn btn-primary'>Three Times</div>",
+            "<div class='choices btn btn-primary' data-correct='true'>Twice</div>",
+            "<div class='choices btn btn-primary'>None</div>"
+        ],
+        correct: false,
+    };
 
-displayQuestion();
+    var questionBank = [question1, question2, question3, question4, question5];
+    var intervalId;
+    var number = 30; // Number of seconds remaining
+    var count = 0; // Keeps track of the index of the currently displaying question.
+    var questionDetails = questionBank[count];
 
-function run() {
-    clearInterval(intervalId);
-    intervalId = setInterval(decrement, 1000);
+    function displayQuestion() {
+        $(`#question`).html(questionDetails.text);
 
-    function decrement() {
-        number--;
-        $(`#time-remaining`).text(number);
-
-        if (number === 0) {
-            alert("Time is up!");
-            stop();
+        for (var i = 0; i < questionDetails.answer.length; i++) {
+            $(`#answer-choices`).append(questionDetails.answer[i]);
         }
     }
-}
 
-function stop() {
-    clearInterval(intervalId);
-}
+    displayQuestion();
 
-// run();
+    function run() {
+        clearInterval(intervalId);
+        intervalId = setInterval(decrement, 1000);
+
+        function decrement() {
+            number--;
+            $(`#time-remaining`).text(number);
+
+            if (number === 0) {
+                alert("Time is up!");
+                stop();
+            }
+        }
+    }
+
+    function stop() {
+        clearInterval(intervalId);
+    }
+
+    // run();
 
 });
