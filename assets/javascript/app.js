c0498b8a03e27874f08444b63b3cdbf0b82c1a60
Jameson Campbell <jcampbell@flexterapp.com>
2019-04-02 13:26:07 -0500

    added comments

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,5 @@
 $(document).ready(function () {
 
-
     var question1 = {
         text: "'The Mountain' is the nickname for which character?",
         answer: [
@@ -9,7 +8,6 @@ $(document).ready(function () {
             "<div class='choices btn btn-primary'>Sandor Clegane</div>",
             "<div class='choices btn btn-primary' data-correct='true'>Gregor Clegane</div>"
         ],
-        correct: false,
     };
 
     var question2 = {
@@ -20,7 +18,6 @@ $(document).ready(function () {
             "<div class='choices btn btn-primary'>Joffrey Baratheon</div>",
             "<div class='choices btn btn-primary'>Stannis Baratheon</div>"
         ],
-        correct: false,
     };
 
     var question3 = {
@@ -31,7 +28,6 @@ $(document).ready(function () {
             "<div class='choices btn btn-primary'>Ser Loras Tyrell</div>",
             "<div class='choices btn btn-primary'>Ser Jamie Lannister</div>"
         ],
-        correct: false,
     };
 
     var question4 = {
@@ -42,7 +38,6 @@ $(document).ready(function () {
             "<div class='choices btn btn-primary'>Tormund Giantsbane</div>",
             "<div class='choices btn btn-primary'>The Night King</div>"
         ],
-        correct: false,
     };
 
     var question5 = {
@@ -53,27 +48,29 @@ $(document).ready(function () {
             "<div class='choices btn btn-primary' data-correct='true'>Twice</div>",
             "<div class='choices btn btn-primary'>None</div>"
         ],
-        correct: false,
     };
 
     var questionBank = [question1, question2, question3, question4, question5];
     var intervalId;
     var number = 10; // Number of seconds remaining
     var count = 0; // Keeps track of the index of the currently displaying question.
-    var wins = 0;
-    var losses = 0;
+    var wins = 0; // Holds the number of correct questions answered
+    var losses = 0; // Holds the number of incorrect questions answered
 
+    $(`#start`).click(startGame);
+    $(`#restart`).click(startGame);
     $(`#game-screen`).css('display', 'none');
     $(`#results-screen`).css('display', 'none');
-    $(`#start`).click(startGame);
 
     function startGame() {
         displayQuestion();
         $(`#splash-screen`).css('display', 'none');	
         $(`#game-screen`).css('display', 'inherit');
-        run();
+        $(`#results-screen`).css('display', 'none');
+        runTimer();
     }
 
+    // displays question and answer choices to user
     function displayQuestion() {
         var questionDetails = questionBank[count];
         $(`#question`).html(questionDetails.text);
@@ -84,6 +81,7 @@ $(document).ready(function () {
         correct();
     }
 
+    // determines if user's answer is correct/incorrect
     function correct() {
         $(`#answer-choices div`).click(function () {
             var correctAnswer = $(this).data('correct');
@@ -107,6 +105,7 @@ $(document).ready(function () {
         });
     }
 
+    // displays number of correct/incorrect anwers in results screen
     function results() {
         $(`#game-screen`).css('display', 'none');
         $(`#results-screen`).css('display', 'inherit');
@@ -114,7 +113,8 @@ $(document).ready(function () {
         $(`#losses`).text(losses);
     }
 
-    function run() {
+    // timer function that decrements
+    function runTimer() {
         clearInterval(intervalId);
         intervalId = setInterval(decrement, 1000);
 
@@ -128,9 +128,11 @@ $(document).ready(function () {
         }
     }
 
+    // displays next question within object
     function nextQuestion() {
         $(`#question`).empty();
         $(`#answer-choices`).empty();
         displayQuestion();
     }
+
 });
